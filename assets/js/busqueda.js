40dc215b2377edf5b0508547ed9b178d010c4486
yezid1878 <ylpc1878@gmail.com>
2024-11-24 08:03:37 -0500

    editando busqueda

--- assets/js/busqueda.js
+++ assets/js/busqueda.js
@@ -12,7 +12,7 @@ $btnBuscar.addEventListener("click", () => {
 
 const cargarUsuarios = async () => {
   try {
-    const res = await fetch(`http://localhost:3000/usuarios`);
+    const res = await fetch(`http://localhost:4000/api/usuarios`);
     console.log(res);
 
     if (res.status === 200) {
@@ -20,11 +20,8 @@ const cargarUsuarios = async () => {
       console.log(json);
 
       json.forEach((el) => {
-        $template.querySelector(".id").textContent = el.id;
         $template.querySelector(".nombre").textContent = el.nombre;
         $template.querySelector(".direccion").textContent = el.direccion;
-        $template.querySelector(".telefono").textContent = el.telefono;
-        $template.querySelector(".email").textContent = el.email;
         let $clone = d.importNode($template, true);
         $fragment.appendChild($clone);
       });
--- assets/js/ingreso.js
+++ assets/js/ingreso.js
@@ -5,6 +5,8 @@ const $ingreso = d.getElementById("ingreso"),
   $usuarios = d.getElementById("usuario"),
   $password = d.getElementById("password");
 
+//  se crea un evento que al dar click en el inicio de seseión con el usuario admin se pueda entrar a la plataforma
+
 $ingreso.addEventListener("click", (e) => {
   let ingreso = () => {
     if (
@@ -17,6 +19,8 @@ $ingreso.addEventListener("click", (e) => {
 
   ingreso();
 });
+
+//  se crea un evento que al dar enter en el inicio de seseión con el usuario admin se pueda entrar a la plataforma
 
 w.addEventListener("keydown", (e) => {
   let ingreso = () => {
